0dc8d196a8ba9a27cabaef4328a628911db28b17
Surya T N <suryatej.nim@yahoo.com>
2022-10-18 15:55:41 +0000

    Implemented JSX elements in EventItem component to render the intended UI

--- src/components/EventItem/index.js
+++ src/components/EventItem/index.js
@@ -1,1 +1,24 @@
-// Write your code here
+import './index.css'
+
+const EventItem = props => {
+  const {itemData, itemClickHandler} = props
+  const {id, imageUrl, name, location} = itemData
+
+  const onItemSelection = () => itemClickHandler(id)
+
+  return (
+    <li className="event-item-container">
+      <button
+        className="event-item-container-button"
+        type="button"
+        onClick={onItemSelection}
+      >
+        <img className="event-item-img" src={imageUrl} alt="event" />
+        <p className="event-item-name">{name}</p>
+        <p className="event-item-location">{location}</p>
+      </button>
+    </li>
+  )
+}
+
+export default EventItem
