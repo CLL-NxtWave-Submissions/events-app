ef24fdf32e7d8f43f72997f8f9d04a43f3f64b30
Surya T N <suryatej.nim@yahoo.com>
2022-10-19 11:11:46 +0000

    Adjusted CSS styling for image JSX element when active and added corresponding logic to set appropriate class names to render the intended UI in EventItem component

--- src/components/EventItem/index.js
+++ src/components/EventItem/index.js
@@ -7,15 +7,17 @@ const EventItem = props => {
   const onItemSelection = () => itemClickHandler(id)
 
   return (
-    <li
-      className={`event-item-container ${isActive ? 'active-event-item' : ''}`}
-    >
+    <li className="event-item-container">
       <button
         className="event-item-container-button"
         type="button"
         onClick={onItemSelection}
       >
-        <img className="event-item-img" src={imageUrl} alt="event" />
+        <img
+          className={`event-item-img ${isActive ? 'active-event-item' : ''}`}
+          src={imageUrl}
+          alt="event"
+        />
         <p className="event-item-name">{name}</p>
         <p className="event-item-location">{location}</p>
       </button>
